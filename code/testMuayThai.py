aee692a83e481ffd6ae3044e90155142ed2370f8
Sun Nattapat <nattapat.sud@ku.th>
2023-03-10 21:35:36 +0700

    update answer of pose

--- code/testMuayThai.py
+++ code/testMuayThai.py
@@ -2,6 +2,50 @@ import pandas as pd
 import numpy as np
 from MuayThai import MuayThai as M
 
+### Step 6
+true_steps = pd.DataFrame([{'step': 1, 'sub_step': 1, 'main_node': '14', 'true_angle': 90, 'operator': '<'},
+{'step': 1, 'sub_step': 2, 'main_node': '13', 'true_angle': 90, 'operator': '<'},
+{'step': 2, 'sub_step': 1, 'main_node': '23', 'true_angle': 90, 'operator': '<'},
+{'step': 3, 'sub_step': 1, 'main_node': '13', 'true_angle': 125, 'operator': '<='},
+{'step': 3, 'sub_step': 2, 'main_node': '11', 'true_angle': 80, 'operator': '>'},
+{'step': 3, 'sub_step': 3, 'main_node': '14', 'true_angle': 90, 'operator': '<='},
+{'step': 3, 'sub_step': 4, 'main_node': '12', 'true_angle': 90, 'operator': '<'},
+{'step': 4, 'sub_step': 1, 'main_node': '14', 'true_angle': 90, 'operator': '>'},
+{'step': 4, 'sub_step': 2, 'main_node': '12', 'true_angle': 90, 'operator': '>'}])
+
+
+cal_steps = pd.DataFrame([{'step': 1, 'sub_step': 1, 'begin_node': '12', 'main_node': '14', 'end_node': '16' , 'axis': 'xy'},
+{'step': 1, 'sub_step': 2, 'begin_node': '11', 'main_node': '13', 'end_node': '15' , 'axis': 'xy'},
+{'step': 2, 'sub_step': 1, 'begin_node': '24', 'main_node': '23', 'end_node': '25' , 'axis': 'yz'},
+{'step': 3, 'sub_step': 1, 'begin_node': '11', 'main_node': '13', 'end_node': '15' , 'axis': 'xy'},
+{'step': 3, 'sub_step': 2, 'begin_node': '13', 'main_node': '11', 'end_node': '23' , 'axis': 'yz'},
+{'step': 3, 'sub_step': 3, 'begin_node': '12', 'main_node': '14', 'end_node': '22' , 'axis': 'xy'},
+{'step': 3, 'sub_step': 4, 'begin_node': '14', 'main_node': '12', 'end_node': '24' , 'axis': 'xy'},
+{'step': 4, 'sub_step': 1, 'begin_node': '12', 'main_node': '14', 'end_node': '16' , 'axis': 'xy'},
+{'step': 4, 'sub_step': 2, 'begin_node': '14', 'main_node': '12', 'end_node': '24' , 'axis': 'yz'}])
+
+'''
+### Step 7
+true_steps = pd.DataFrame([{'step': 1, 'sub_step': 1, 'main_node': '14', 'true_angle': 90, 'operator': '<'},
+{'step': 1, 'sub_step': 2, 'main_node': '13', 'true_angle': 90, 'operator': '<'},
+{'step': 2, 'sub_step': 1, 'main_node': '12', 'true_angle': 75, 'operator': '>'},
+{'step': 2, 'sub_step': 2, 'main_node': '14', 'true_angle': 130, 'operator': '>='},
+{'step': 3, 'sub_step': 1, 'main_node': '24', 'true_angle': 15, 'operator': '<'},
+{'step': 3, 'sub_step': 2, 'main_node': '12', 'true_angle': 80, 'operator': '<'},
+{'step': 4, 'sub_step': 1, 'main_node': '24', 'true_angle': 15, 'operator': '<'}])
+
+
+cal_steps = pd.DataFrame([{'step': 1, 'sub_step': 1, 'begin_node': '12', 'main_node': '14', 'end_node': '16' , 'axis': 'xy'},
+{'step': 1, 'sub_step': 2, 'begin_node': '11', 'main_node': '13', 'end_node': '15' , 'axis': 'xy'},
+{'step': 2, 'sub_step': 1, 'begin_node': '14', 'main_node': '12', 'end_node': '24' , 'axis': 'yz'},
+{'step': 2, 'sub_step': 2, 'begin_node': '12', 'main_node': '14', 'end_node': '16' , 'axis': 'yz'},
+{'step': 3, 'sub_step': 1, 'begin_node': '12', 'main_node': '24', 'end_node': '26' , 'axis': 'xy'},
+{'step': 3, 'sub_step': 2, 'begin_node': '14', 'main_node': '12', 'end_node': '24' , 'axis': 'yz'},
+{'step': 4, 'sub_step': 1, 'begin_node': '12', 'main_node': '24', 'end_node': '26' , 'axis': 'xy'}])
+'''
+
+'''
+### Step 15
 true_steps = pd.DataFrame([{'step': 1, 'sub_step': 1, 'main_node': '14', 'true_angle': 90, 'operator': '<'},
 {'step': 1, 'sub_step': 2, 'main_node': '13', 'true_angle': 90, 'operator': '<'},
 {'step': 2, 'sub_step': 1, 'main_node': '14', 'true_angle': 90, 'operator': '>'},
@@ -13,7 +57,7 @@ true_steps = pd.DataFrame([{'step': 1, 'sub_step': 1, 'main_node': '14', 'true_a
 {'step': 4, 'sub_step': 2, 'main_node': '26', 'true_angle': 90, 'operator': '<'},
 {'step': 4, 'sub_step': 3, 'main_node': '12', 'true_angle': 90, 'operator': '<'},
 {'step': 4, 'sub_step': 4, 'main_node': '11', 'true_angle': 90, 'operator': '<'},
-{'step': 4, 'sub_step': 5, 'main_node': '14', 'true_angle': 90, 'operator': '<'},
+{'step': 4, 'sub_step': 5, 'main_node': '14', 'true_angle': 100, 'operator': '<'},
 {'step': 4, 'sub_step': 6, 'main_node': '13', 'true_angle': 90, 'operator': '<'}])
 
 
@@ -28,16 +72,17 @@ cal_steps = pd.DataFrame([{'step': 1, 'sub_step': 1, 'begin_node': '12', 'main_n
 {'step': 4, 'sub_step': 2, 'begin_node': '24', 'main_node': '26', 'end_node': '28' , 'axis': 'yz'},
 {'step': 4, 'sub_step': 3, 'begin_node': '14', 'main_node': '12', 'end_node': '24' , 'axis': 'xy'},
 {'step': 4, 'sub_step': 4, 'begin_node': '13', 'main_node': '11', 'end_node': '23' , 'axis': 'xy'},
-{'step': 4, 'sub_step': 5, 'begin_node': '12', 'main_node': '14', 'end_node': '16' , 'axis': 'xy'},
+{'step': 4, 'sub_step': 5, 'begin_node': '12', 'main_node': '14', 'end_node': '22' , 'axis': 'xy'},
 {'step': 4, 'sub_step': 6, 'begin_node': '11', 'main_node': '13', 'end_node': '15' , 'axis': 'xy'}])
-
+'''
 
 clip_number = 4
 
-df = pd.read_csv('new/data/15.csv')
+df = pd.read_csv('../video/test_csv/test.csv')
 del df['Unnamed: 0']
 
-clip_df = df[df['clip_name'] == '15_{}'.format(clip_number)]
+#clip_df = df[df['clip_name'] == '15_{}'.format(clip_number)]
+clip_df = df
 
 m = M(clip_df, 4, true_steps, cal_steps)
 #print(m.get_data_size())
